266b967fad5ecc369019c296684f7099a12556f2
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-03-15 18:39:08 +0100

    "Updated version of WordBST that now uses Java's Collection package TreeMap.

--- src/WordBST.java
+++ src/WordBST.java
@@ -1,114 +1,45 @@
 import java.io.Console;
 import java.util.Scanner;
-
+import java.util.Map;
+import java.util.TreeMap;
 public class WordBST {
-    // WordNode: Indre klasse for en node i sÃ¸ketreet
-    class WordNode
+
+/*** Oppdaterte versjon av WordBST som benytter heller javas bibliotek for TreeMap-datastrukturen***/
+    Map<String, Integer> T;
+
+    public WordBST()
     {
-        /*** oppgave 1 ***/
-        String word;
-        WordNode right, left = null;
-        int counter;
-
-        public WordNode(String word, WordNode right, WordNode left){
-            this.word = word;
-            this.right = right;
-            this.left = left;
-            counter ++;
-        }
-
-        public String print(){
-            return  word + ": " + counter;
-        }
+       T = new TreeMap<>();
     }
 
-    private WordNode rot; // Roten i hele sÃ¸ketreet
-    private int n;        // Antall noder i hele treet
-
-    // WordBST(): KonstruktÃ¸r som lager et tomt sÃ¸ketre
-    public void WordBST()
-    {
-        rot = null;
-        n = 0;
-    }
-
-    // size(): Antall ord som er lagret i treet
     public int size()
     {
-        return n;
+       return T.size();
     }
 
 
     /*** oppgave 2 ***/
     private void insert(String ord) {
-        if (rot == null) {
-            rot = new WordNode(ord, null, null);
-            n++;
-            return;
-        }
-        WordNode current = rot;
-        while (true) {
-            int compareResult = ord.compareTo(current.word);
-
-            if (compareResult == 0) {
-                current.counter++;
-
-                return;
-            } else if (compareResult < 0) {
-                if (current.left == null) {
-                    current.left = new WordNode(ord, null, null);
-                    n++;
-                    return;
-                }
-                current = current.left;
-            } else {
-                if (current.right == null) {
-                    current.right = new WordNode(ord, null, null);
-                    n++;
-                    return;
-                }
-                current = current.right;
-            }
-        }
+        T.put(ord, T.getOrDefault(ord, 0) + 1);
     }
 
 
 
-    // search(): SÃ¸k etter et ord. Skriv ut ordet og ordfrekvensen
-    // hvis det finnes i sÃ¸ketreet.
     public void search(String ord)
     {
         /*** oppgave 3 ***/
-        WordNode current = rot;
-        while (current != null){
-            int compareResult = ord.compareTo(current.word);
-            if(compareResult == 0){
-                System.out.println(current.print());
-                return;
-            }
-            if(compareResult < 0){
-                current = current.left;
-            }
-            else current = current.right;
-        }
-        System.out.println("Word not found");
+      if(T.containsKey(ord)){
+          System.out.println(ord + " :" + T.get(ord));
+      }
+      else {
+          System.out.println("The tree does not contain the word");
+      }
+
     }
 
-    // print(): Alfabetisk utskrift av hele sÃ¸ketreet. Kaller en
-    // rekursiv metode som gjÃ¸r selve utskriften.
-    public void print()
-    {
-        print(rot);
-    }
-
-    // print(): Rekursiv utskrift av hele sÃ¸ketreet med rot i "rot"
-    private void print(WordNode rot)
-    {
-        /*** oppgave 4 ***/
-        if(rot != null){
-            print(rot.left);
-            System.out.println(rot.print());
-            print(rot.right);
+    public void print() {
+        for (Map.Entry<String, Integer> entry : T.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 
@@ -116,27 +47,25 @@ public class WordBST {
     // main(): Testprogram
     public static void main (String argv[])
     {
-        // Leser filnavn fra bruker
+
         Scanner scan = new Scanner(System.in);
         System.out.print("File? ");
         String fileName = scan.next();
 
-        // Oppretter ordleser og tomt sÃ¸ketre
+
         WordReader wR = new WordReader(fileName);
         WordBST wBST = new WordBST();
 
-        // Leser alle ordene pÃ¥ filen og legger inn i treet
         String ord = wR.nextWord();
         while (ord != null)
         {
             wBST.insert(ord);
             ord = wR.nextWord();
         }
-        // Skriver ut antall ulike ord som fantes i filen
+
         System.out.println(wBST.size() + " unique words " +
                 "read from file " + fileName);
 
-        // Menyvalg for Ã¥ teste programmet
         int valg = 0;
         while(valg != 3)
         {
